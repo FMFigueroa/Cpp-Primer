f38603b91351ed8814dbff9f7713323fc0142a3e
FMFigueroa <felixmanuelfigueroa@gmail.com>
2022-06-28 19:11:27 -0300

    Chapter 2

--- Chapter 2/9 Const Qualifer/main.cpp
+++ Chapter 2/9 Const Qualifer/main.cpp
@@ -118,5 +118,24 @@ int main ()
     //No podemos usar este puntero para cambiar el valor del objeto al que apunta, 
     //ni la direccion almacenada a la que apunta, en este caso el objeto pi .
     *pip = 2.72; // error: pip is a pointer to const
+
+
+    //Top Level
+    /*
+        Tenga en cuenta que los tipos de puntero, a diferencia de la mayoría otros tipos,
+        pueden tener constantes de nivel superior y de nivel bajo de forma independiente:
+    */
+
+    int i = 0;
+    int *const p1 = &i; // we can’t change the value of p1; const is top-level
+    const int ci = 42; // we cannot change ci; const is top-level
+    const int *p2 = &ci; // we can change p2; const is low-level
+    const int *const p3 = p2; // right-most const is top-level, left-most is not
+    const int &r = ci; // const in reference types is always low-level
+
+
+
+
+
 }
 
