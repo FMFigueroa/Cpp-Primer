d995fe2cf9c35125457f84a23701c526c8957e24
FMFigueroa <felixmanuelfigueroa@gmail.com>
2022-06-29 23:48:24 -0300

    Chapter 2

--- Chapter 2/10 Estructuras de datos/main.cpp
+++ Chapter 2/10 Estructuras de datos/main.cpp
@@ -9,4 +9,7 @@ struct Sales_data {
 std::string bookNo;
 unsigned units_sold = 0;
 double revenue = 0.0;
-};
+}; //El punto y coma es necesario porque podemos definir variables después del cuerpo de la clase.
+//Example:
+Sales_data accum, trans, *salesptr;
+
