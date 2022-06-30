8b6445f7a4a158219f7c5c1413005c4b53039c7a
FMFigueroa <felixmanuelfigueroa@gmail.com>
2022-06-29 23:56:14 -0300

    Chapter 2

--- Chapter 2/10 Estructuras de datos/main.cpp
+++ Chapter 2/10 Estructuras de datos/main.cpp
@@ -1,15 +1,14 @@
 //En C++ definimos nuestros propios tipos de datos definiendo una clase.
 // Los tipos de biblioteca string, istream y ostream están todos definidos como clases.
 
-//Sales_data es una version de Sales_item
+
 #include <iostream>
-
-
-struct Sales_data {
-std::string bookNo;
-unsigned units_sold = 0;
-double revenue = 0.0;
-}; //El punto y coma es necesario porque podemos definir variables después del cuerpo de la clase.
-//Example:
-Sales_data accum, trans, *salesptr;
-
+#include <string>
+#include "Sales_data.h"
+int main()
+{
+Sales_data data1, data2;
+// code to read into data1 and data2
+// code to check whether data1 and data2 have the same ISBN
+// and if so print the sum of data1 and data2
+}
