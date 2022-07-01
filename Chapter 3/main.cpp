77cf7d02e963b2f10f1da29bb2be41d76cf94140
FMFigueroa <felixmanuelfigueroa@gmail.com>
2022-07-01 17:28:31 -0300

    Chapter 3 v0.1

--- Chapter 3/main.cpp
+++ Chapter 3/main.cpp
@@ -16,6 +16,7 @@ int main() {
 }
 */
 
+/*
 #include <iostream>
 
 
@@ -36,3 +37,57 @@ cout << "The sum of " << v1 << " and " << v2 << " is " << v1 + v2 << endl;
 return 0;
 
 }
+*/
+
+//Diferentes manera de inicializar un String
+
+/*
+
+    #include <string>
+
+    using std::string;
+
+    int main () 
+    {
+        string s1; // default initialization; s1 is the empty string
+        string s2 = s1; // s2 is a copy of s1
+        string s3 = "hiya"; // s3 is a copy of the string literal
+        string s4(10, ’c’); // s4 is cccccccccc
+    }
+    
+*/
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+// Note: #include and using declarations must be added to compile this code
+int main()
+{
+
+//example Hello World!  ==> s1 = Hello y s2 = world!
+string s1, s2; // empty string
+cin >> s1 >> s2; // read a whitespace-separated string into s
+cout << s1 << " " << s2 << endl; // write s to the output
+
+system("pause");
+return 0;
+
+}
+
+//Lectura de un número desconocido de cadenas
+/*
+    #include <iostream>
+    #include <string>
+
+    using namespace std;
+
+    int main()
+    {
+    string word;
+    while (cin >> word)           // read until end-of-file
+    cout << word << endl;       // write each word followed by a new line
+    return 0;
+    }
+*/
