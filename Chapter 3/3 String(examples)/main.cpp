335fdc36deadbb2b29172bf429a0f2ec91d1884c
FMFigueroa <felixmanuelfigueroa@gmail.com>
2022-07-13 12:56:48 -0300

    Chapter 3 v0.1

--- Chapter 3/3 String(examples)/main.cpp
+++ Chapter 3/3 String(examples)/main.cpp
@@ -15,22 +15,19 @@ int main(){
 
     //Ejemplo 1 (Recorriendo cada caracter de la cadena):
     string str("some string");
-
     for (auto c : str) // for every char in str
     cout << c << endl; // print the current character followed by a newline
 
 
     //Ejemplo 2 (Recorriendo cada caracter de la cadena y lo Comparamos):
     string s("Hello World!!!");
-
     // punct_cnt has the same type that s.size returns;
     decltype(s.size()) punct_cnt = 0;
-
     for (auto c : s) // for every char in s
         if (ispunct(c)) // if the character is punctuation
             ++punct_cnt; // increment the punctuation counter
-
     cout << punct_cnt << " punctuation characters in " << s << endl;
+
 
     //Ejemplo 3 (Recorriendo cada caracter de la cadena para cambiar caracteres):
     string txt("Hello World!!!");
@@ -39,6 +36,12 @@ int main(){
         ref = toupper(ref); // c is a reference, so the assignment changes the char in s
     cout << txt << endl;
 
+
+    //Ejemplo 4 (Accediendo a un caracter especifico con el operador de subindice []):
+
+    if (!str.empty()) // make sure there’s a character in s[0]
+    str[0] = toupper(str[5]); // assign a new value to the first character in s
+    cout << str << endl; // print the first character in str
 
 
     getch();
