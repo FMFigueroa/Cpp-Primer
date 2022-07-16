76ee1ef8785a0ef91bb0edb35bc3be9b210d8c1e
FMFigueroa <felixmanuelfigueroa@gmail.com>
2022-07-16 10:42:47 -0300

    Chapter 3 v0.1

---
+++ Chapter 3/4 Vector/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include<conio.h>
+
+
+using namespace std;
+using std::vector;
+
+int main() {
+
+
+    vector<int> ivec; // ivec holds objects of type int
+    //vector<Sales_item> Sales_vec; // holds Sales_items
+    vector<vector<string>> file; // vector whose elements are vectors
+
+
+    getch();
+    return 0;
+}
