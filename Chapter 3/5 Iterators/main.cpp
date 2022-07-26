52d7d74604632787911b39c959779dde9fe3ab64
FMFigueroa <felixmanuelfigueroa@gmail.com>
2022-07-25 21:29:28 -0300

    Chapter 3 v0.1

--- Chapter 3/5 Iterators/main.cpp
+++ Chapter 3/5 Iterators/main.cpp
@@ -1,15 +1,26 @@
 #include <iostream>
-#include<conio.h>
-
-
+#include <conio.h>
+#include <vector>
 
 using namespace std;
 
-int main() {
+int main()
+{
+   //================  Miembros denominados begin y end  ====================
+   // the compiler determines the type of b and e;
+   // b denotes the first element and e denotes one past the last element in v
+   vector<unsigned> v(10, 0);
 
-   
+   auto b = v.begin(),
+        e = v.end(); // b and e have the same type
 
-
+   string s("some string");
+   if (s.begin() != s.end())
+   {                       // make sure s is not empty
+      auto it = s.begin(); // it denotes the first character in s
+      *it = toupper(*it);  // make that character uppercase
+   }
+   cout << s << endl; // Some string
    getch();
    return 0;
 }
