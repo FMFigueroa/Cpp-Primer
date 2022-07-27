d9df4da92925b7a08d849ca9d3a2a37b424244b1
FMFigueroa <felixmanuelfigueroa@gmail.com>
2022-07-27 13:32:19 -0300

    Chapter 3 v0.1

--- Chapter 3/5 Iterators/main.cpp
+++ Chapter 3/5 Iterators/main.cpp
@@ -10,10 +10,10 @@ int main()
    // the compiler determines the type of b and e;
    // b denotes the first element and e denotes one past the last element in v
    vector<unsigned> v(10, 0);
-
    auto b = v.begin(),
         e = v.end(); // b and e have the same type
 
+   // Operaciones de iterador
    string s("some string");
    if (s.begin() != s.end())
    {                       // make sure s is not empty
@@ -21,6 +21,12 @@ int main()
       *it = toupper(*it);  // make that character uppercase
    }
    cout << s << endl; // Some string
+
+   // Mover iteradores de un elemento a otro
+   for (auto it = s.begin(); it != s.end() && !isspace(*it); ++it)
+      *it = toupper(*it); // capitalize the current character
+   cout << s << endl;     // SOME string
+
    getch();
    return 0;
 }
